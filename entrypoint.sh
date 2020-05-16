1416736c65915278afe0b8a95f07ef730ba6dd49
James <13262021+james-coder@users.noreply.github.com>
2020-05-15 23:33:56 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,13 @@
 /usr/bin/ssh -V
 
 # Add Github host key authentication.
-/usr/bin/ssh -T -oStrictHostKeyChecking=accept-new git@github.com || true
+#/usr/bin/ssh -T -oStrictHostKeyChecking=accept-new git@github.com || true
 
 #ls -al ~/.ssh
 #ssh-add -l
 
-echo "$SSH_AUTH_SOCK"
-ls -altr $SSH_AUTH_SOCK
+#echo "$SSH_AUTH_SOCK"
+#ls -altr $SSH_AUTH_SOCK
 
 /composer/vendor/bin/phpstan --version
 
@@ -28,8 +28,8 @@ if [ "$REQUIRE_DEV" = "true" ]; then
     NO_DEV=""
 fi
 
-#COMPOSER_COMMAND="composer install --no-scripts --no-progress $NO_DEV $IGNORE_PLATFORM_REQS"
-COMPOSER_COMMAND="composer install --no-progress -vvv $NO_DEV $IGNORE_PLATFORM_REQS"
+COMPOSER_COMMAND="composer install --no-scripts --no-progress $NO_DEV $IGNORE_PLATFORM_REQS"
+#COMPOSER_COMMAND="composer install --no-progress -vvv $NO_DEV $IGNORE_PLATFORM_REQS"
 echo "::group::$COMPOSER_COMMAND"
 $COMPOSER_COMMAND
 echo "::endgroup::"
