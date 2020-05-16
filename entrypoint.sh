f136a8c1980e289b93c1fb8638008242308c30ef
James <13262021+james-coder@users.noreply.github.com>
2020-05-15 23:41:58 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,5 @@
 #!/bin/sh -l
 
-# SSH must be 7.6 or newer for "accept-new".
-/usr/bin/ssh -V
-
-# Add Github host key authentication.
-#/usr/bin/ssh -T -oStrictHostKeyChecking=accept-new git@github.com || true
-
-#ls -al ~/.ssh
-#ssh-add -l
-
-#echo "$SSH_AUTH_SOCK"
-#ls -altr $SSH_AUTH_SOCK
-
-/composer/vendor/bin/phpstan --version
-
-echo "::group::Installed PHPStan extensions"
-composer show | grep phpstan
-echo "::endgroup::"
-
 IGNORE_PLATFORM_REQS=""
 if [ "$CHECK_PLATFORM_REQUIREMENTS" = "false" ]; then
     IGNORE_PLATFORM_REQS="--ignore-platform-reqs"
@@ -29,8 +11,13 @@ if [ "$REQUIRE_DEV" = "true" ]; then
 fi
 
 COMPOSER_COMMAND="composer install --no-scripts --no-progress $NO_DEV $IGNORE_PLATFORM_REQS"
-#COMPOSER_COMMAND="composer install --no-progress -vvv $NO_DEV $IGNORE_PLATFORM_REQS"
 echo "::group::$COMPOSER_COMMAND"
 $COMPOSER_COMMAND
 echo "::endgroup::"
-/composer/vendor/bin/phpstan $*
+
+echo "::group::Installed PHPStan extensions"
+composer show | grep phpstan
+echo "::endgroup::"
+
+./vendor/bin/phpstan --version
+./vendor/bin/phpstan $*
